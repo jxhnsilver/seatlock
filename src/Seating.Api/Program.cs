c4cc79f53bea5e79241f2e60bef85f4fa2d5b09a
Ilia Fedin <infedinwork@gmail.com>
2026-09-15 17:26:57 +0300

    feat(seating): setup postgresql connection and add ef core migrations

--- src/Seating.Api/Program.cs
+++ src/Seating.Api/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Seating.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<SeatingDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello! I'm Seating service.");

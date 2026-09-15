b7b4d203d0f70a7498a8088f7220b171d9547eac
Ilia Fedin <infedinwork@gmail.com>
2026-09-15 14:36:08 +0300

    feat(seating): add unit-tests for Hall entity

---
+++ tests/Seating.Api.UnitTests/Entities/HallTests.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using Seating.Api.Entities;
+using Seating.Api.Enums;
+
+namespace Seating.Api.UnitTests.Entities
+{
+    public sealed class HallTests
+    {
+        [Fact]
+        public void Constructor_WithValidParameters_ShouldInitializeCorrectly()
+        {
+            string expectedName = "Красный зал";
+            var expectedType = HallType.Standard;
+
+            var hall = new Hall(expectedName, expectedType);
+
+            hall.Name.Should().Be(expectedName);
+            hall.Type.Should().Be(expectedType);
+            hall.Status.Should().Be(HallStatus.Active);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void Constructor_WithInvalidName_ShouldThrowArgumentException(string invalidName)
+        {
+            var type = HallType.Standard;
+
+            Action act = () => _ = new Hall(invalidName, type);
+
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("*Название зала не может быть пустым.*")
+                .And.ParamName.Should().Be("name"); ;
+        }
+
+        [Fact]
+        public void SetName_WithValidName_ShouldUpdateName()
+        {
+            var hall = new Hall("Красный зал", HallType.Standard);
+            string newName = "Синий зал";
+
+            hall.SetName(newName);
+
+            hall.Name.Should().Be(newName);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void SetName_WithInvalidName_ShouldThrowArgumentException(string invalidName)
+        {
+            var hall = new Hall("Красный зал", HallType.Standard);
+
+            Action act = () => hall.SetName(invalidName);
+
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("*Название зала не может быть пустым.*")
+                .And.ParamName.Should().Be("newName"); ;
+        }
+
+        [Fact]
+        public void ChangeStatus_WithNewStatus_ShouldUpdateStatus()
+        {
+            var hall = new Hall("Синий зал", HallType.Standard);
+            var newStatus = HallStatus.Maintenance;
+
+            hall.ChangeStatus(newStatus);
+
+            hall.Status.Should().Be(newStatus);
+        }
+    }
+}

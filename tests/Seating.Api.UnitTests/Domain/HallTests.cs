c2cf78d410329c25076674bc3eaa0bcdc4ba2d3f
Ilia Fedin <infedinwork@gmail.com>
2026-09-17 21:51:22 +0300

    refactor(tests): move entity tests to Domain folder in unit-test project
    - Moved HallTests and SeatTests from Entities/ to Domain/
    - Updated namespaces to match new folder structure

--- tests/Seating.Api.UnitTests/Domain/HallTests.cs
+++ tests/Seating.Api.UnitTests/Domain/HallTests.cs
@@ -1,19 +1,21 @@
 ﻿using FluentAssertions;
 using Seating.Api.Domain.Halls;
-using Seating.Api.Entities;
 
-namespace Seating.Api.UnitTests.Entities
+namespace Seating.Api.UnitTests.Domain
 {
     public sealed class HallTests
     {
         [Fact]
         public void Constructor_WithValidParameters_ShouldInitializeCorrectly()
         {
+            // Arrange
             string expectedName = "Красный зал";
             var expectedType = HallType.Standard;
 
+            // Act
             var hall = new Hall(expectedName, expectedType);
 
+            // Assert
             hall.Name.Should().Be(expectedName);
             hall.Type.Should().Be(expectedType);
             hall.Status.Should().Be(HallStatus.Active);
@@ -25,10 +27,13 @@ namespace Seating.Api.UnitTests.Entities
         [InlineData(null)]
         public void Constructor_WithInvalidName_ShouldThrowArgumentException(string invalidName)
         {
+            // Arrange
             var type = HallType.Standard;
 
+            // Act
             Action act = () => _ = new Hall(invalidName, type);
 
+            // Assert
             act.Should().Throw<ArgumentException>()
                 .WithMessage("*Название зала не может быть пустым.*")
                 .And.ParamName.Should().Be("name"); ;
@@ -37,11 +42,14 @@ namespace Seating.Api.UnitTests.Entities
         [Fact]
         public void SetName_WithValidName_ShouldUpdateName()
         {
+            // Arrange
             var hall = new Hall("Красный зал", HallType.Standard);
             string newName = "Синий зал";
 
+            // Act
             hall.SetName(newName);
 
+            // Assert
             hall.Name.Should().Be(newName);
         }
 
@@ -51,10 +59,13 @@ namespace Seating.Api.UnitTests.Entities
         [InlineData(null)]
         public void SetName_WithInvalidName_ShouldThrowArgumentException(string invalidName)
         {
+            // Arrange
             var hall = new Hall("Красный зал", HallType.Standard);
 
+            // Act
             Action act = () => hall.SetName(invalidName);
 
+            // Assert
             act.Should().Throw<ArgumentException>()
                 .WithMessage("*Название зала не может быть пустым.*")
                 .And.ParamName.Should().Be("newName"); ;
@@ -63,11 +74,14 @@ namespace Seating.Api.UnitTests.Entities
         [Fact]
         public void ChangeStatus_WithNewStatus_ShouldUpdateStatus()
         {
+            // Arrange
             var hall = new Hall("Синий зал", HallType.Standard);
             var newStatus = HallStatus.Maintenance;
 
+            // Act
             hall.ChangeStatus(newStatus);
 
+            // Assert
             hall.Status.Should().Be(newStatus);
         }
     }
--- tests/Seating.Api.UnitTests/Domain/SeatTests.cs
+++ tests/Seating.Api.UnitTests/Domain/SeatTests.cs
@@ -1,21 +1,23 @@
 ﻿using FluentAssertions;
 using Seating.Api.Domain.Seats;
-using Seating.Api.Entities;
 
-namespace Seating.Api.UnitTests.Entities
+namespace Seating.Api.UnitTests.Domain
 {
     public sealed class SeatTests
     {
         [Fact]
         public void Constructor_WithValidParameters_ShouldInitializeCorrectly()
         {
+            // Arrange
             int expectedHallId = 1;
             int expectedRowNumber = 5;
             int expectedSeatNumber = 12;
             var expectedType = SeatType.Single;
 
+            // Act
             var seat = new Seat(expectedHallId, expectedRowNumber, expectedSeatNumber, expectedType);
 
+            // Assert
             seat.HallId.Should().Be(expectedHallId);
             seat.RowNumber.Should().Be(expectedRowNumber);
             seat.SeatNumber.Should().Be(expectedSeatNumber);
@@ -27,12 +29,15 @@ namespace Seating.Api.UnitTests.Entities
         [InlineData(-1)]
         public void Constructor_WithInvalidHallId_ShouldThrowArgumentException(int invalidHallId)
         {
+            // Arrange
             int rowNumber = 5;
             int seatNumber = 12;
             var type = SeatType.Single;
 
+            // Act
             Action act = () => _ = new Seat(invalidHallId, rowNumber, seatNumber, type);
 
+            // Assert
             act.Should().Throw<ArgumentException>()
                 .WithMessage("*Идентификатор зала должен быть больше нуля.*")
                 .And.ParamName.Should().Be("hallId");
@@ -43,12 +48,15 @@ namespace Seating.Api.UnitTests.Entities
         [InlineData(-1)]
         public void Constructor_WithInvalidRowNumber_ShouldThrowArgumentException(int invalidRowNumber)
         {
+            // Arrange
             int hallId = 1;
             int seatNumber = 12;
             var type = SeatType.Single;
 
+            // Act
             Action act = () => _ = new Seat(hallId, invalidRowNumber, seatNumber, type);
 
+            // Assert
             act.Should().Throw<ArgumentException>()
                 .WithMessage("*Номер ряда должен быть больше нуля.*")
                 .And.ParamName.Should().Be("rowNumber");
@@ -59,12 +67,15 @@ namespace Seating.Api.UnitTests.Entities
         [InlineData(-1)]
         public void Constructor_WithInvalidSeatNumber_ShouldThrowArgumentException(int invalidSeatNumber)
         {
+            // Arrange
             int hallId = 1;
             int rowNumber = 5;
             var type = SeatType.Single;
 
+            // Act
             Action act = () => _ = new Seat(hallId, rowNumber, invalidSeatNumber, type);
 
+            // Assert
             act.Should().Throw<ArgumentException>()
                 .WithMessage("*Номер места должен быть больше нуля.*")
                 .And.ParamName.Should().Be("seatNumber");

9a8359caef155bf3d114a38e58a95722de1cb9d7
Ilia Fedin <infedinwork@gmail.com>
2026-09-17 21:46:43 +0300

    feat(seating): add Hall DTOs for service contract

---
+++ src/Seating.Api/Dtos/CreateHallDto.cs
@@ -0,0 +1,6 @@
+﻿using Seating.Api.Domain.Halls;
+
+namespace Seating.Api.Dtos
+{
+    public sealed record CreateHallDto(string Name, HallType Type);
+}
---
+++ src/Seating.Api/Dtos/HallDto.cs
@@ -0,0 +1,6 @@
+﻿using Seating.Api.Domain.Halls;
+
+namespace Seating.Api.Dtos
+{
+    public sealed record HallDto(int Id, string Name, HallType Type, HallStatus Status);
+}

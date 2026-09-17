68ba3b80722b1e80d79fcb3cd558fe0dbd19eb14
Ilia Fedin <infedinwork@gmail.com>
2026-09-17 12:34:22 +0300

    feat(building-blocks): add shared domain exceptions

---
+++ src/BuildingBlocks/Exceptions/ConflictException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BuildingBlocks.Exceptions
+{
+    public sealed class ConflictException : Exception
+    {
+        public ConflictException(string message) : base(message) { }
+    }
+}

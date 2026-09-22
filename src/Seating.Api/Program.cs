d08071bc0544dbdb55e01121a58ee34bc9726936
Ilia Fedin <infedinwork@gmail.com>
2026-09-22 16:15:55 +0300

    feat(swagger): implement swagger support

--- src/Seating.Api/Program.cs
+++ src/Seating.Api/Program.cs
@@ -1,6 +1,9 @@
 using BuildingBlocks.ErrorHandling;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 using Seating.Api.Data;
+using Seating.Api.Services;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +15,33 @@ builder.Services.AddScoped<IHallService, HallService>();
 
 builder.Services.AddErrorHandling();
 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Seatlock",
+        Version = "v1",
+    });
+});
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    }); ;
+
 var app = builder.Build();
 
 app.UseExceptionHandler();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Seatlock v1");
+    });
+}
 
 using (var scope = app.Services.CreateScope())
 {
